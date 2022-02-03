3ee6a33a06ec02715fe12bce0b9bb8497ed3605e
huskpurin <huskpurin@gmail.com>
2022-02-03 21:44:06 +0800

    import local module hello

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const hello = require('@node-js-getting-started/hello').default
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/hello', (req, res) => res.send(hello('world')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
